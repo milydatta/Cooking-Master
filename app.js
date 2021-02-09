d424e2b4d7380b202db8a24abd7632b4b0335314
milydatta <55607701+milydatta@users.noreply.github.com>
2021-02-09 17:15:12 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
-.then(res => res.json())
-.then(data => displayMeals(data.meals));
-
-const displayMeals = meals =>{
-    const mealsDiv = document.getElementById('meal-area');
-   meals.forEach(meal => {
-       const mealDiv = document.createElement('div');
-        const mealInfo = `
-             <h3 class="meal-name">${meal.name}</h3>
-          
-             <button onclick="displayMealDetail('${meal.name}')">Details</button>
-            `;
-           mealDiv.innerHTML = mealInfo;
-           mealsDiv.appendChild(mealDiv);
-    });
-}
-
-const displayMealDetail = name => {
-    const url = `https://restcountries.eu/rest/v2/name/${name}`
+const searchMeals = () =>{
+    const searchText = document.getElementById('show_meal').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=all`
+    //load data
     fetch(url)
     .then(res => res.json())
-    .then(data => renderMealInfo(data[0]));
-} 
-
-const renderMealInfo = meal => {
-    const mealDiv = document.getElementById('mealDetail');
-    mealDiv.innerHTML = `
-       <h1>${meal.name}</h1>
-       <p>Ingredients: ${ingredients.name}</p>
-       <img src="${meal}">
-    `
+    .then(data => displayMeals(data.meals))
 }
-const searchBtn = document.getElementById("search_button");
-searchBtn.addEventListener('click',function(){
-   const mealArea = document.getElementById("meal-area");
-    mealArea.style.display = "block";
-})
+const displayMeals = meals => {
+     console.log(meals)
+     const mealContainer = document.getElementById('meal-container');
+     mealContainer.innerHTML = '';
+     meals.forEach(meal => {
+        const mealDiv = document.createElement('div');
+         mealDiv.className = 'single-result row align-items-center my-3 p-3'
+         mealDiv.innerHTML = `
+               <div class="col-md-9">
+                    <h3 class="meal-name">${meal.strMeal}</h3>
+                    <img src="${meal.strMealThumb}" alt="" width="200" height="200">
+               </div>
+               <div class="col-md-3 text-md-right text-center">
+                    <button onclick="getIngredients('${meal.strMealThumb}','${meal.strIngredient1}','${meal.strIngredient2}','${meal.strIngredient3}','${meal.strIngredient4}','${meal.strIngredient5})" class="btn btn-success">Ingredients</button>
+               </div>
+               `;
+        mealContainer.appendChild(mealDiv);
+     })
+}
+const getIngredients = (strMealThumb, strIngredient1, strIngredient2, strIngredient3,strIngredient4,strIngredient5) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=all`
+      fetch(url)
+     .then(res => res.json())
+     .then(data => displayIngredients(data.strIngredient))
+ }
+
+const displayIngredients = Ingredients => {
+     const ingredientsDiv = document.getElementById('meal-ingredients')
+     ingredientsDiv.innerText = Ingredients;   
+}
